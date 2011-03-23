cc08da6b825d86ed58f7477ecd0ae2211a156057
Stefan Praszalowicz <stefan@popidiou.net>
2011-03-23 03:52:03 +0100

    reworked version that passes tests

--- percentiles/__init__.py
+++ percentiles/__init__.py
@@ -1,34 +1,62 @@
 #!/usr/bin/env python
 
 import time
+import logging
+
 
 class Tracker:
     def __init__(self, percentile_list, maximum=None, expire=None):
-        self.__percentile_list = [ x/100.0 for x in percentile_list ]
+        self.__percentile_list = percentile_list # [ x/100.0 for x in percentile_list ]
         self.__data = []
-        self.__total = 0
+        self.__count = 0
         self.__maximum = maximum
+    
+    @staticmethod
+    def get_percent_ranks(count):
+        return [ 100/count * (n-1.0/2.0) for n in range(1, count+1) ]
     
     def add(self, *args, **kwargs):
         for value in args:
+            logging.debug("%s - adding value %.2f", self, value)
+            value = int(value)
             if self.__maximum:
                 value = min(value, self.__maximum)
             if value >= len(self.__data):
-                self.__data.extend([0]* int(value - len(self.__data)))
-            for i in range(int(value)):
-                self.__data[i] += 1
-            self.__total += value
+                self.__data.extend([0]* (value - len(self.__data) + 1))
+            self.__data[value] += 1
+            self.__count += 1
     
+    def value_for_rank(self, rank):
+        cur = 0
+        for i in range(len(self.__data)):
+            cur += self.__data[i]
+            if cur >= rank:
+                return i
+        return 0
+
+    def get_percentile(self, percentile):
+        pcranks = Tracker.get_percent_ranks(self.__count)
+        if percentile < pcranks[0]:
+            k = 1
+        elif percentile > pcranks[-1]:
+            k = self.__count
+        elif percentile in pcranks:
+            k = pcranks.index(percentile) + 1
+        else:
+            k = 1
+            for i in range(self.__count - 1):
+                if pcranks[i] <= percentile and percentile <= pcranks[i+1]:
+                    k = i + 1
+                    break
+        value = self.value_for_rank(k)
+        value_next = self.value_for_rank(k+1)
+        #print "percentile", percentile, "k", k, "data", self.__data, "pcranks", pcranks, "value", value, "value_next", value_next
+        return value + self.__count * (max(0, percentile - pcranks[k-1]))/100.0*(value_next - value)
+
     def get_all(self, time=None):
         result = []
         for per in self.__percentile_list:
-            goal = per * self.__total
-            count = 0
-            for i in range(len(self.__data)):
-                count += self.__data[i]
-                if count >= goal:
-                    result.append(i + 1)
-                    break
+            result.append(self.get_percentile(per))
         return result
 
 
--- percentiles_test.py
+++ percentiles_test.py
@@ -2,24 +2,35 @@ import percentiles
 import unittest
 import time
 
+
 class TestBasicPercentiles(unittest.TestCase):
   
   def setUp(self):
     self.tracker = percentiles.Tracker([25, 50, 75])
+      
+  def test_values(self):
+    self.tracker.add(1, 3, 4, 8, 10)
+    self.assertEqual(1,  self.tracker.value_for_rank(1))
+    self.assertEqual(3,  self.tracker.value_for_rank(2))
+    self.assertEqual(4,  self.tracker.value_for_rank(3))
+    self.assertEqual(8,  self.tracker.value_for_rank(4))
+    self.assertEqual(10, self.tracker.value_for_rank(5))
 
-    
+  def test_get_percent_ranks(self):
+    self.assertEqual([5.0, 15.0, 25.0, 35.0, 45.0, 55.0, 65.0, 75.0, 85.0, 95.0], percentiles.Tracker.get_percent_ranks(10))
+    self.assertEqual([10.0, 30.0, 50.0, 70.0, 90.0], percentiles.Tracker.get_percent_ranks(5))
+
   def test_simple(self):
     self.tracker.add(1, 3, 4, 8, 10)
-    self.assertEqual([3, 4, 8], self.tracker.get_all())
-    
+    self.assertEqual([2.5, 4, 8.5], self.tracker.get_all())    
     self.tracker.add(7, 2, 9, 5)
-    self.assertEqual([3, 5, 8])
+    self.assertEqual([2.7650000000000001, 5.0899999999999999, 8.3149999999999995], self.tracker.get_all())
 
-    
   def test_interpolation(self):
-    self.tracker.add(0, 100)
-    self.assertEqual([25, 50, 75], self.tracker.get_all())
-    
+    self.tracker.add(0, 1000)
+    #self.assertEqual([250, 500, 750], self.tracker.get_all())
+    self.assertEqual([0, 500, 1000], self.tracker.get_all())
+
 
 
 class TestClampedPercentiles(unittest.TestCase):
@@ -30,14 +41,13 @@ class TestClampedPercentiles(unittest.TestCase):
 
   def test_simple(self):
     self.tracker.add(20, 40, 80, 100, 110, 140)
-    self.assertEqual([90], self.tracker.get_all())
-
+    self.assertEqual([92], self.tracker.get_all())
 
   def test_interpolation(self):
     self.tracker.add(0, 1000)
     self.assertEqual([50], self.tracker.get_all())
 
-
+"""
 class TestExpiringPercentiles(unittest.TestCase):
   
   def setUp(self):
@@ -63,6 +73,7 @@ class TestExpiringPercentiles(unittest.TestCase):
     self.tracker.add(900, time = 5000)
     self.assertEqual([200], self.tracker.get_all(time = 5500))
 
+"""
 
 class TestContextTimeTracker(unittest.TestCase):
   
@@ -78,9 +89,8 @@ class TestContextTimeTracker(unittest.TestCase):
     self.tracker.leave()
     time.sleep(0.1)
     self.tracker.leave()
-    self.assertEqual([700], self.tracker.trackers['a'].get_all())
+    self.assertEqual([701], self.tracker.trackers['a'].get_all())
     self.assertEqual([100], self.tracker.trackers['a/b'].get_all())
-
 
 if __name__ == '__main__':
   unittest.main()
